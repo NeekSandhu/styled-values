c4f6552fdd8afa6345651390446e16382f1324d9
Neek Sandhu <neek.sandhu@outlook.com>
2019-06-04 15:56:38 -0700

    switch back to old module.exports interop

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+function styledValues(predicate, defaultValue) {
+  return props => {
+    const valuesMap = predicate(props);
+    if (!valuesMap || typeof valuesMap !== 'object') {
+      throw new TypeError('Expected an object with CSS prop-values mapped to booleans');
+    }
+    for (const key in valuesMap) {
+      if (valuesMap[key]) {
+        return key;
+      }
+    }
+    return defaultValue;
+  };
+}
 
-export default function styledValues(predicate, defaultValue) {
-    return props => {
-      const valuesMap = predicate(props);
-      if(!valuesMap || typeof valuesMap !== 'object') {
-        throw new TypeError('Expected an object with CSS prop-values mapped to booleans');
-      }
-      for(const key in valuesMap) {
-        if(valuesMap[key]) {
-          return key;
-        }
-      }
-      return defaultValue;
-    };
-  }
+module.exports = styledValues.default = styledValues
